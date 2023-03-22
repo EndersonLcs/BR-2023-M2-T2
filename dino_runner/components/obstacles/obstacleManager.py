882a680f75b1fd5591c25f00de8d7f25579fa55d
Enderson Lucas <endersonlucas27@gmail.com>
2023-03-22 11:40:51 -0300

    AULA 4: power ups

--- dino_runner/components/obstacles/obstacleManager.py
+++ dino_runner/components/obstacles/obstacleManager.py
@@ -21,12 +21,15 @@ class ObstacleManager:
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
-                #game.score = 0
-                #game.game_speed = 20
-                game.death_count += 1
-                pygame.time.delay(500)
-                game.playing = False
-                break
+                if not game.player.has_power_up:
+                    #game.score = 0
+                    #game.game_speed = 20
+                    game.death_count += 1
+                    pygame.time.delay(500)
+                    game.playing = False
+                    break
+                else:
+                    self.obstacles.remove(obstacle)
 
     def draw(self, screen):
         for obstacle in self.obstacles:
